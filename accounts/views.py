ffc5497f39b2d1d5fc8e4bee90ed081aecc7e7c7
Ajay Rathod <rathodajay60@gmail.com>
2020-04-22 14:06:45 +0530

    login and logout complete

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,55 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
-from .forms import OrderForm
+from .forms import OrderForm, CreateUserForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+def registerPage(request):
+	if request.user.is_authenticated:
+		return redirect('home')
+	else:
+
+		form = CreateUserForm()
+		if request.method == 'POST':
+			form = CreateUserForm(request.POST)
+			if form.is_valid():
+				form.save()
+				user = form.cleaned_data.get('username')
+				messages.success(request, 'Acount created for' + user )
+				return redirect('login')
+
+	context = {'form':form}
+	return render(request, "accounts/register.html", context)
+
+def loginPage(request):
+	if request.user.is_authenticated:
+		return redirect('home')
+	else:
+
+
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password = request.POST.get('password')
+
+			user = authenticate(request, username=username, password=password)
+			if user is not None:
+				login(request, user)
+				return redirect('home')
+			else:
+				messages.info(request, 'Username or password is incorrect')
+		context = {}
+		return render(request, "accounts/login.html", context)
+
+def logoutPage(request):
+	logout(request)
+	return redirect('login')
+
+@login_required(login_url = 'login')
 def home(request):
 	orders = Order.objects.all()
 	customers = Customer.objects.all()
@@ -16,11 +62,11 @@ def home(request):
 	context = {'orders':orders, 'customers':customers,
 	 'total_orders':total_orders, 'delivered':delivered, 'pending':pending}
 	return render(request, "accounts/dashbord.html", context)
-
+@login_required(login_url = 'login')
 def products(request):
 	products = Product.objects.all()
 	return render(request, "accounts/products.html", {'products':products})
-
+@login_required(login_url = 'login')
 def customer(request, pk_test):
 	customer = Customer.objects.get(id = pk_test)
 
@@ -29,9 +75,8 @@ def customer(request, pk_test):
 
 	context = {'customer':customer, 'orders':orders, 'order_count':order_count}
 	return render(request, "accounts/customer.html", context)
-
-def createOrder(request):
-
+@login_required(login_url = 'login')
+def createOrder(request):	
 	form = OrderForm()
 	if request.method == 'POST':
 		# print('Printing POST:', request.POST)
@@ -41,7 +86,7 @@ def createOrder(request):
 			return redirect('/')
 	context = {'form':form}
 	return render(request, "accounts/order_form.html", context)
-
+@login_required(login_url = 'login')
 def updateOrder(request, pk):
 
 	order = Order.objects.get(id=pk)
@@ -53,7 +98,7 @@ def updateOrder(request, pk):
 			return redirect('/')
 	context = {'form':form}
 	return render(request, "accounts/order_form.html", context)
-
+@login_required(login_url = 'login')
 def deleteOrder(request, pk):
 
 	order = Order.objects.get(id=pk)
